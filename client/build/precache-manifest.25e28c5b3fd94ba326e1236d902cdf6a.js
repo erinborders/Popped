5d378838a58c8f3b4fbc4b9e4abafe7fbd1dba83
Erin Borders <borders16@gmail.com>
2019-09-01 11:24:00 -0400

    installed dependencies

---
+++ client/build/precache-manifest.25e28c5b3fd94ba326e1236d902cdf6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc220deeb1d525559175063bd103140",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70eb2495e66cee2f5896",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "70eb2495e66cee2f5896",
+    "url": "/static/js/main.d1200044.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
