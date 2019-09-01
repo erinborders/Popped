614f3bba853a8f6cb5a81f86a6de82771927869b
Erin Borders <borders16@gmail.com>
2019-09-01 11:55:25 -0400

    created shop pop up model

---
+++ models/shopPopUp.js
@@ -0,0 +1,71 @@
+const mongoose = require('./connection.js')
+
+// info that will be in the Shop Pop Ups
+
+const ShopSchema = new mongoose.Schema({
+    shopName: {
+        type: String,
+        required: true
+    },
+    product: {
+        type: String,
+        description: String,
+        price: Number,
+        required: true
+    },
+    price: {
+        type: String,
+        enum: ['$', '$$', '$$$', '$$$$'],
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        time: String,
+        default: Date.now
+    },
+    locationId: mongoose.Types.ObjectId
+})
+
+const ShopCollection = mongoose.model('Shop', ShopSchema)
+
+function getAllShops() {
+    return ShopCollection.find()
+  }
+  
+  function getShopsByLocationId (locationId) {
+    return ShopCollection.find({locationId: locationId})
+  }
+  
+  function addShop (shop) {
+    return ShopCollection.create(shop)
+  }
+  
+  function getOneShop (shopId) {
+    return ShopCollection.findById(shopId)
+  }
+  
+  function editShop (shopId, newShop) {
+    return ShopCollection.findByIdAndUpdate(shopId, newShop)
+  }
+  
+  function deleteShop (shopId) {
+    return ShopCollection.findByIdAndDelete(shopId)
+  }
+  
+  function deleteAllShops () {
+    return ShopCollection.deleteMany()
+  }
+
+  module.exports = {
+      getAllShops,
+      getShopsByLocationId,
+      addShop,
+      getOneShop,
+      editShop,
+      deleteShop,
+      deleteAllShops
+  }
