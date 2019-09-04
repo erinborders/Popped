2545e3c03dac08debee233b2448ea07c31702829
Erin Borders <borders16@gmail.com>
2019-09-04 11:40:05 -0400

    finished get requests for home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Home extends Component {
     state = {
@@ -6,11 +7,74 @@ export default class Home extends Component {
         foodPopUps: [],
         shopPopUps: []
     }
-    
+
+    componentDidMount() {
+        this.fetchLocations()
+            .then(() => {
+                this.fetchFoodPopUps()
+            })
+            .then(() => {
+                this.fetchShopPopUps()
+            })
+    }
+
+    fetchLocations = () => {
+        axios.get('/api/locations')
+            .then(res => {
+                this.setState({locations: res.data})
+            })
+    }
+
+    fetchFoodPopUps = () => {
+        axios.get('/api/food')
+            .then(res => {
+                this.setState({foodPopUps: res.data})
+            })
+    }
+
+    fetchShopPopUps = () => {
+        axios.get('/api/shops')
+            .then(res => {
+                this.setState({shopPopUps: res.data})
+            })
+    }
+
     render() {
+        let locationsList = this.state.locations.map(location => {
+            return(
+                <div>
+                    <p>{location.neighborhood}</p>
+                </div>
+            )
+        })
+
+        let foodList = this.state.foodPopUps.map(foodPopUp => {
+            return(
+                <div>
+                    <p>{foodPopUp.restaurantName}</p>
+                </div>
+            )
+        })
+
+        let shopList = this.state.shopPopUps.map(shopPopUp => {
+            return(
+                <div>
+                    <p>{shopPopUp.shopName}</p>
+                </div>
+            )
+        })
+
         return (
             <div>
-                
+                <div>
+                    {locationsList}
+                </div>
+                <div>
+                    {foodList}
+                </div>
+                <div>
+                    {shopList}
+                </div>
             </div>
         )
     }
