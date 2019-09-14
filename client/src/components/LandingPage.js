eebbf2d973f321c5d64a39679fa56f3a51e641b7
Erin Borders <borders16@gmail.com>
2019-09-14 13:29:42 -0400

    added logo and changed subtitle

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -4,13 +4,10 @@ export default class LandingPage extends Component {
     render() {
         return (
             <div>
-                <div id="home-container">
-                    <div id="inner">
-                        {/* Popped logo */}
-                        <p class="subtitle">All pop ups in one place</p>
-                    </div>
-                    <div id="animation">
-                        {/* circular gradient expanding outward animation */}
+                <div id="landing-page-container">
+                    <div id="circle">
+                        <h1 className="logo">Popped</h1>
+                        <p className="subtitle">Atlanta pop ups in one place</p>
                     </div>
                 </div>
             </div>
