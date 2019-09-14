3ab63eb9d3e1b66a8dc3b51785de7022f9aedfca
Erin Borders <borders16@gmail.com>
2019-09-14 16:26:25 -0400

    added paper and container to search bar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import { Container, Paper } from '@material-ui/core'
 
 export default class SearchBar extends Component {
     render() {
         return (
-            <div>
-                <h2>Search</h2>
-            </div>
+            <Container>
+                <Paper id="search-bar">
+                    <h2>Search</h2>
+                </Paper>
+            </Container>
         )
     }
 }
