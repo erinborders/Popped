2ea4ac85ae9233f6be7f8492d097a39b2547f15a
Erin Borders <borders16@gmail.com>
2019-09-14 12:10:25 -0400

    created landing page component and initial containers

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                <div id="home-container">
+                    <div id="inner">
+                        {/* Popped logo */}
+                        <p class="subtitle">All pop ups in one place</p>
+                    </div>
+                    <div id="animation">
+                        {/* circular gradient expanding outward animation */}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
