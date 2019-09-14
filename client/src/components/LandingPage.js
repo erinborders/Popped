81a5131ec3a233f9119172208653a9371bac161b
Erin Borders <borders16@gmail.com>
2019-09-14 15:00:19 -0400

    added background image and its animation

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Atlanta from '../images/atlanta-arial.jpg'
 
 export default class LandingPage extends Component {
     render() {
         return (
             <div>
                 <div id="landing-page-container">
+                    <div id="image-container">
+                        <img id="landing-page-image" src={Atlanta} />
+                    </div>
                     <div id="circle">
                         <h1 className="logo">Popped</h1>
                         <p className="subtitle">Atlanta pop ups in one place</p>
