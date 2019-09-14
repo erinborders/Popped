263db8cda5167705d11c6b81e7ec1478c5181763
Erin Borders <borders16@gmail.com>
2019-09-14 16:16:18 -0400

    created initial search side bar

---
+++ client/src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SearchBar extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Search</h2>
+            </div>
+        )
+    }
+}
