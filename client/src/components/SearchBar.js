6bbf5ed0712ea9c05c7999cc7125c5d9727a1c5e
Erin Borders <borders16@gmail.com>
2019-09-14 17:33:22 -0400

    added handle change and handle submit functionality to search bar input

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react'
 import { Container, Input, List, ListItem, ListItemIcon, ListItemText, Paper } from '@material-ui/core'
-
+import SearchIcon from '@material-ui/icons/Search'
+import axios from 'axios'
 
 export default class SearchBar extends Component {
+    state = {
+        events: [],
+        zipcode: ''
+    }
+
+    handleChange = evt => {
+        let zipcode = evt.target.value 
+        this.setState({zipcode})
+    }
+
+    handleSubmit = evt => {
+        evt.preventDefault()
+
+        axios.get(`/api/fetchEvents/?zipcode=${this.state.zipcode}`)
+            .then(res => {
+                this.setState({events: res.data.events})
+            })
+    }
+
     render() {
         return (
-            <Container>
+            <Container id="search-bar-container">
                 <Paper id="search-bar">
                     <h2>Search</h2>
-                    <div>
+                    <form>
+                        <SearchIcon />
                         <Input
                             placeholder="zipcode"
+                            name="zipcode"
+                            onChange={this.handleChange}
+                            value={this.state.zipcode}
                         />
-                    </div>
+                    </form>
                 </Paper>
             </Container>
         )
