41f5a48a52b53ceba57f6ebb5086b4e24e2085b7
Erin Borders <borders16@gmail.com>
2019-09-14 13:29:19 -0400

    added landing page component to routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './components/Home.js'
+import LandingPage from './components/LandingPage.js'
 import './App.css';
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={LandingPage}/>
         </Switch>
       </Router>
     </div>
