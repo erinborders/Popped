035195ce4b3178633a85d1eb787bb897463f5177
Erin Borders <borders16@gmail.com>
2019-10-02 11:45:02 -0400

    post and delete handlers functional but still have issues

--- controllers/events.js
+++ controllers/events.js
@@ -17,22 +17,32 @@ eventRouter.get('/', (req, res) => {
         })
 })
 
+//TO DO: figure out what's wrong with this
+eventRouter.delete('/', (req, res) => {
+    eventApi.deleteAllEvents()
+    .catch(err => {
+        console.log('error', err)
+    })
+})
+
 //updating the array of events from eventbrite
 eventRouter.post('/', (req, res) => {
+    //figure out a way to delete previous events
     // eventApi.deleteAllEvents()
+    
     let events = [];
     fetch(`https://www.eventbriteapi.com/v3/events/search/?q=pop%20up&location.address=Atlanta&expand=venue,category&token=${process.env.PRIVATE_TOKEN}`)
     .then(res => res.json())
     .then(json => {
         events = json
-        // console.log('events array', events)
-    })
-    .then(() => {
         eventApi.updateAllEvents(events)
     })
-    // .then(newEvents => {
-    //     res.json(newEvents)
+    // .then(() => {
+    //     eventApi.updateAllEvents(events)
     // })
+    .then(newEvents => {
+        res.json(newEvents)
+    })
     .catch(err => {
         console.log('error', err)
     })
