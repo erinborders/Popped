c3d606752ea6706164e8917fb483b7751eaa75ce
Erin Borders <borders16@gmail.com>
2019-10-08 14:27:07 -0400

    cleaned up code

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-/* Step 1
- *
- * Import needed packages
- *
- */
+
 const express = require('express')
 const app = express()
 
 const dotenv = require('dotenv');
 dotenv.config();
-/* Step 2
- * 
- * import routers from controllers/
- *
- */
+
 const { locationRouter } = require('./controllers/location.js')
 const { foodRouter } = require('./controllers/foodPopUp.js')
 const { shopRouter } = require('./controllers/shopPopUp.js')
@@ -20,36 +12,16 @@ const { eventRouter }  = require('./controllers/events.js')
 const { eventBrite, getEventsByCategory, grabAllCategories, getEventsByZipcode } = require('./controllers/eventbritePopUp.js')
 const { addEmailToNewsletter } = require('./controllers/mailChimp.js')
 
-/* Step 3
- *
- * Register middleware...
- */
 
-/* Step 3.a
- * ...to parse the body of the HTTP requests from a URL encoded string 
- */
 app.use(express.urlencoded({extended: true}))
 
-/* Step 3.b 
- *
- * ...to parse the body of the HTTP requests from a JSON string  
- */
+
 app.use(express.json())
 
 
-/* Step 3.c
- *
- * use the `./client/build` directory to host static resources such as css and
- * image files 
- */
+
 app.use(express.static(`${__dirname}/client/build`))
 
-
-/* Step 4
- *
- * add router for the application to use. The first argument is a prefix to all
- * the paths defined in the router.
- */
 app.use('/api/locations', locationRouter)
 app.use('/api/food', foodRouter)
 app.use('/api/shops', shopRouter)
@@ -100,10 +72,7 @@ app.get('/api/fetchAllCategories', (req, res) => {
 })
 
 //add email to mailchimp newsletter
-app.post('/api/addSubscriber', (req, res) => {
-    //TO DO: make sure emailaddress is correct and check to make sure you need all these .thens
-    
-    console.log('request is reaching server')
+app.post('/api/addSubscriber', (req, res) => { 
     const email = req.query.emailAddress 
     const name = req.query.name
 
@@ -119,27 +88,15 @@ setInterval(function() {
     http.get("http://poppedpopups.herokuapp.com");
 }, 300000); // every 5 minutes 
 
-/* Step 5
- *
- * add catch all route to serve up the built react app for any request not made to our
- * /api/... routes.
- */
+
 app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
-/* Step 6
- *
- * Set the port the server is to run on
- *
- * NOTE: keep these lines at the bottom of the file 
- */
+
 const PORT = process.env.PORT || 3001
 
-/* Step 7
- *
- * Start the server
- */
+
 app.listen(PORT, () => {
     console.log(`App is listening on PORT ${PORT}`)
 })
