2a977227b54a299bcec095f08c957590d43eb3b1
Erin Borders <borders16@gmail.com>
2019-10-11 11:22:51 -0400

    minor changes

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Input, List, ListItem, ListItemIcon, ListItemText, Paper } from '@material-ui/core'
+import { Container, Input, Paper } from '@material-ui/core'
 import SearchIcon from '@material-ui/icons/Search'
 import axios from 'axios'
 
@@ -11,15 +11,15 @@ export default class SearchBar extends Component {
             <Container id="search-bar-container">
                 <Paper id="search-bar">
                     <h2>Search For Pop Ups in Your Area</h2>
-                    <form onSubmit={this.props.handleSearchSubmit}>
-                        <SearchIcon />
-                        <Input
-                            placeholder="zipcode"
-                            name="zipcode"
-                            onChange={this.props.handleSearchChange}
-                            value={this.props.zipcode}
-                        />
-                    </form>
+                        <form onSubmit={this.props.handleSearchSubmit}>
+                            <SearchIcon />
+                            <Input
+                                placeholder="zipcode"
+                                name="zipcode"
+                                onChange={this.props.handleSearchChange}
+                                value={this.props.zipcode}
+                            />
+                        </form>
                 </Paper>
             </Container>
         )
